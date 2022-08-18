765cf6d7a07aa0e81c094cb492e7501d75b366d7
Sergioaiki07 <109544635+Sergioaiki07@users.noreply.github.com>
2022-08-18 09:33:55 -0400

    Update main.py
    ingreso de comentarios

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import time
 from colorama import Fore
 #Interfaz usuario
 
-print("Host conectados: ", func.obtener_ips())
-inicial_ips=func.obtener_ips()
+print("Host conectados: ", func.obtener_ips()) #obtiene los host conectados como primera accion
+inicial_ips=func.obtener_ips() #obtiene las ip de los host 
 while True: #ciclo infinito
-    cantidad_host = func.host_registrados()
-    lista_actual_ip = func.obtener_ips()
+    cantidad_host = func.host_registrados() #obtiene datos de host
+    lista_actual_ip = func.obtener_ips() #obtienelos host en el tiempo actual
     lista_actual = func.listar_host()
-    prueba = set(lista_actual_ip)
-
-    diferencias = set(lista_actual_ip) - set(inicial_ips)
+    diferencias = set(lista_actual_ip) - set(inicial_ips) #Compara los vectores, si existe una diferencia mostrara que host se adiciono
     if diferencias:
-        print(Fore.RED+"Se detectarón los siguientes host conectados!!", diferencias)
+        print(Fore.RED+"Se detectarón los siguientes host conectados!!", diferencias) #imprime los host adicionados
 
-    time.sleep(5)
+    time.sleep(5) #Analisis cada 5 segundos 
 
 
